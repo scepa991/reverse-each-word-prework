b07fd45d690f37be7a3079a7c291a919b1b48247
Stefan Scepanovic <scepa91@icloud.com>
2019-05-09 23:55:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   original_array = string.split(" ")
   return_array = []
   original_array.each do |string|
-    return_array << string.reverse 
+    return_array << string.reverse
   end
   return_array.join(" ")
 end
